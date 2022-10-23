b9923cc275682f6c23eefe79196f4185b797b86d
Your Name <you@example.com>
2022-10-23 19:18:58 +0000

    finished 04-02hw

--- eligibility.js
+++ eligibility.js
@@ -1,17 +1,84 @@
-console.log(mdc);
+// console.log(mdc);
 
 const MDCTextField = mdc.textField.MDCTextField;
 const textFields = [].map.call(
-  document.querySelectorAll(".mdc-text-field"),
-  function (el) {
-    return new MDCTextField(el);
-  }
+    document.querySelectorAll(".mdc-text-field"),
+    function (el) {
+        return new MDCTextField(el);
+    }
 );
 
 const MDCCheckbox = mdc.checkbox.MDCCheckbox;
 const checkboxes = [].map.call(
-  document.querySelectorAll(".mdc-checkbox"),
-  function (el) {
-    return new MDCCheckbox(el);
-  }
+    document.querySelectorAll(".mdc-checkbox"),
+    function (el) {
+        return new MDCCheckbox(el);
+    }
 );
+
+// console.log(document.forms[0]);
+const myForm = document.forms[0];
+const signUpButton = myForm[8];
+
+const [fullNameInput, usernameInput, enterPasswordInput, confirmPasswordInput, ageInput, birthDateInput, legalCheckbox, termsCheckbox] = myForm;
+// console.dir(signUpButton)
+
+function validateEligibility(event){
+  event.preventDefault();
+  const fullNameVal = fullNameInput.value;
+  const usernameVal = usernameInput.value;
+  const enterPasswordVal = enterPasswordInput.value;
+  const confirmPasswordVal = confirmPasswordInput.value;
+  const ageVal = ageInput.value;
+  const birthDateVal = birthDateInput.value;
+  const legalVal = legalCheckbox.checked;
+  const termsVal = termsCheckbox.checked;
+
+  // console.log(legalVal, termsVal);
+
+  console.log(`Full Name: ${fullNameVal}`)
+  console.log(`Username: ${usernameVal}`)
+  console.log(`Enter Password: ${enterPasswordVal}`)
+  console.log(`Confirm Password: ${confirmPasswordVal}`)
+  console.log(`Age: ${ageVal}`)
+  console.log(`Birth Date: ${birthDateVal}`)
+
+  if (legalVal){
+    console.log("The user has checked the legal checkbox");
+  }
+  else{
+    console.log("The user has not checked the legal checkbox");
+  }
+  if (termsVal){
+    console.log("The user has checked the terms checkbox");
+  }
+  else{
+    console.log("The user has not checked the terms checkbox");
+  }
+
+  fields = [fullNameVal, usernameVal, enterPasswordVal, confirmPasswordVal, ageVal, birthDateVal,]
+
+  let fieldsFilled = true;
+  for(var i=0; i<fields.length; i++) {
+      if(fields[i] === ""){
+        fieldsFilled = false;
+        break;
+        }
+  }
+
+  const passwordsSame = enterPasswordVal === confirmPasswordVal;
+  const validAge = ageVal >= 13;
+  const bothChecked = legalVal && termsVal;
+    
+  if (passwordsSame && validAge && bothChecked && fieldsFilled) {
+    console.log("The user is eligible");
+  }
+  else{
+    console.log("The user is ineligible");
+  }
+
+}
+
+signUpButton.addEventListener("click", validateEligibility);
+
+
--- table.js
+++ table.js
@@ -1,3 +1,20 @@
+const MDCTextField = mdc.textField.MDCTextField;
+const textFields = [].map.call(
+    document.querySelectorAll(".mdc-text-field"),
+    function (el) {
+        return new MDCTextField(el);
+    }
+);
+
+const MDCCheckbox = mdc.checkbox.MDCCheckbox;
+const checkboxes = [].map.call(
+    document.querySelectorAll(".mdc-checkbox"),
+    function (el) {
+        return new MDCCheckbox(el);
+    }
+);
+
+
 const suits = ["♠", "♡", "♢", "♣"];
 const ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Knave", "Queen", "King"];
 
@@ -18,10 +35,74 @@ function getRandomCard() {
     return deck[randomIndex];
 }
 
-console.log(deck)
+function dealRandomCard() {
+    dealToDisplay(getRandomCard());
+  }
+
+function dealToDisplay(card){
+    const newCard = document.createElement("li"); // creates a new list item
+    newCard.setAttribute("data-blackjack-value", rankToValue(card.rank)); // sets the data-blackjack-value attribute to second argument
+    newCard.innerText = `${rankToWord(card.rank)} of ${suitToWord(card.suit)}`; // sets the text of the newCard
+    const playerCards = document.querySelector("#playersCards ol"); // selects the appropriate section
+    playerCards.appendChild(newCard); // adds playerCards
+}
+
+function rankToWord(rank){
+    const mapRanksToWords = { 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten", };
+    return (typeof(rank) === "number") ? mapRanksToWords[rank] : rank;
+}
+
+function suitToWord(suit) {
+    const mapSuitsToWords = {"♠": "Spades", "♡": "Hearts", "♢": "Diamonds", "♣": "Clubs", "": "Mystery",};
+    return mapSuitsToWords[suit];
+}
+
+function rankToValue(rank){
+    const mapRanksToValues = { "Ace": "11/1", "Knave": "10", "Queen": "10", "King": "10", "Face Down" : "?",}
+    return (rank in mapRanksToValues) ? mapRanksToValues[rank] : rank.toString() ;
+}
+
+// console.log(deck)
 const hitButton = document.querySelector("#hit-button");
-hitButton.addEventListener("click", () => {
-    const randomCard = getRandomCard();
-    console.log(`Your card is ${randomCard.rank} of ${randomCard.suit}. Did you bust?`)
-    console.log(randomCard);
-});
+hitButton.addEventListener("click", dealRandomCard
+
+// () => { console.log(dealRandomCard());
+    // const randomCard = getRandomCard();
+    // console.log(`Your card is ${randomCard.rank} of ${randomCard.suit}. Did you bust?`)
+    // console.log(randomCard);
+// }
+);
+
+
+var bankroll = 2022;
+const players_actions_section = document.querySelector("#playersActions");
+const betting_section = document.querySelector("#betting");
+const betting_form = document.forms[0];
+const bet_button = betting_form[1];
+bet_button.addEventListener("click", makeWager);
+
+
+function addBankroll() {
+    // document.getElementById("bankroll").innerHTML = `$${bankroll}`;
+}
+
+function getBankroll(){ return bankroll }
+function setBankroll(newBalance){ bankroll = newBalance; }
+function timeToBet() { 
+    players_actions_section.classList.add("hidden");
+    betting_section.classList.remove("hidden");
+    document.getElementById("bankroll").innerHTML = `$${getBankroll()}`;
+}
+
+function makeWager(event){
+    event.preventDefault();
+    console.log(betting_form[0].value);
+    timeToPlay()
+}
+
+function timeToPlay() {
+    players_actions_section.classList.remove("hidden");
+    betting_section.classList.add("hidden");
+}
+
+
